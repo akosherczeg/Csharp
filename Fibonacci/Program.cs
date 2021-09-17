3b528e0b6b98f53f9d0b9281cba5924c4f680ae2
akosherczeg <akosherczeg97@gmail.com>
2021-09-17 10:37:51 +0200

    Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Fibonacci(int a)
+        {
+            int first = 9;
+            int second = -5;
+            int third = 0;
+            for (int i = 2; i < a; i++)
+            {
+                third = first + second;
+                first = second;
+                second = third;
+            }
+            Console.WriteLine(third);
+        } 
+        static void Main(string[] args)
+        {
+            Fibonacci(30);
+        }
+    }
+}
